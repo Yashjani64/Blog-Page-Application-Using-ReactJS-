ed1a4fcbb5dffe2637f708daecc20871d02b6cc2
Yashjani64 <99891723+Yashjani64@users.noreply.github.com>
2022-12-03 23:57:11 -0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import "./App.css";
+import Home from "./pages/Home";
+import Blog from "./pages/Blog";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blog" element={<Blog />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
