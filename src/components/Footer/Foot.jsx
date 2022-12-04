72cf7cdaac95796afc22aca320d88a0791e714e5
Yashjani64 <99891723+Yashjani64@users.noreply.github.com>
2022-12-04 00:05:23 -0500

    Add files via upload

---
+++ src/components/Footer/Foot.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Foot.css";
+
+const Foot = () => {
+  return (
+    <div>
+      <footer>
+        <p>Company © W3docs. All rights reserved.</p>
+      </footer>
+    </div>
+  );
+};
+
+export default Foot;
